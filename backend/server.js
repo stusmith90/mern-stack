eda9bdca384d39b5bfe942c11a0fc25fe002df49
Stuart Smith <stuart.ingersoll-smith@cherrythinking.com>
2019-07-25 15:35:09 +0100

    add list for developer tasks

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,11 @@ connection.once('open', () => {
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
+const devTaskRouter = require('./routes/devTask');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+app.use('/dev-tasks', devTaskRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
