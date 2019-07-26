b888e7ac3af88391b871f1c3add07fb9fb0484f0
Stuart Smith <stuart.ingersoll-smith@cherrythinking.com>
2019-07-26 10:40:04 +0100

    developer tasks added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./Components/Navbar";
-import ExercisesList from "./Components/ExercisesList";
+import ExercisesDevList from "./Components/ExercisesDevList";
 import EditExercise from "./Components/EditExercise";
+import EditDevTask from "./Components/EditDevTask";
 import CreateExercise from "./Components/CreateExercise";
+import CreateDevTask from "./Components/CreateDevTask";
 import CreateUser from "./Components/CreateUser";
 
 function App() {
@@ -14,9 +16,11 @@ function App() {
       <div className="container">
       <Navbar />
       <br/>
-      <Route path="/" exact component={ExercisesList} />
+      <Route path="/" exact component={ExercisesDevList} />
       <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/edit-dev-task/:id" component={EditDevTask} />
       <Route path="/create" component={CreateExercise} />
+      <Route path="/create-dev-task" component={CreateDevTask} />
       <Route path="/user" component={CreateUser} />
       </div>
     </Router>
